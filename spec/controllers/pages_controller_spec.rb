9e9b080df37f99b281c56cb7fcb059f03f21d2ba
Akarika <roux.remy.m@gmail.com>
2016-03-07 17:38:21 +0100

    Fin des pages statiques

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,18 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
-
+  render_views
   describe "GET #home" do
     it "returns http success" do
       get :home
-      expect(response).to have_http_status(:success)
+      expect(response).to be_success
     end
+  it "should have the right title" do
+    get :home
+    expect(response.body).to have_selector('title', :text => 'Home')
   end
-
+end
   describe "GET #contact" do
     it "returns http success" do
       get :contact
-      expect(response).to have_http_status(:success)
+      expect(response).to be_success
     end
   end
 
