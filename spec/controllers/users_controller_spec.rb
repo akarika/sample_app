1520fc54af5f2c3bdf612695dee5e2115863ad11
Akarika <roux.remy.m@gmail.com>
2016-03-08 09:41:01 +0100

    fin chap5

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  render_views
+
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+    it "devrait avoir le titre adéquat" do
+      get :new
+      expect(response.body).to have_title(@base_title +' | Inscription')
+    end
+end
+end
